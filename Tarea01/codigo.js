8e91d528ca2ed40fbf685f4f3dbe1e97b82539fc
Rodolfo D. Arce S <info@rodolfoarce.com>
2020-09-09 21:53:11 -0400

    Tarea 01 - Ejercicio 05

--- Tarea01/codigo.js
+++ Tarea01/codigo.js
@@ -1,2 +1,9 @@
-var mi_nombre = "Rodolfo Arce";
-document.getElementById("prueba").innerHTML = "Hola cómo estas " + mi_nombre;
+var funcionario = {
+    nombre: "Rodolfo",
+    apellido: "Arce",
+    edad: 25,
+    direccion: "Mi calle 123",
+    telefono: "595 021 123 456"
+};
+
+document.getElementById("funcionario").innerHTML = funcionario.nombre + " " + funcionario.apellido + " con " + funcionario.edad + " años de edad vive en " + funcionario.direccion + " puede localizarlo al teléfono " + funcionario.telefono;
