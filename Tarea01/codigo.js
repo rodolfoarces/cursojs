6e4be3287b643135ecbcd0e4bf00619cff923fbf
Rodolfo D. Arce S <info@rodolfoarce.com>
2020-09-09 22:10:13 -0400

    Tarea 01 - Ejercicio 10

--- Tarea01/codigo.js
+++ Tarea01/codigo.js
@@ -1,8 +1,12 @@
 // Creado por Rodolfo Arce
-
+var mi_nombre = "Rodolfo";
+var persona = { 
+    nombre: "Daniel",
+    direccion: "Mi calle 123"
+}
+var mi_edad = 25;
 /*
 Ejemplo realizado para el curso de SNPP de JavaScript
 con varias lineas de comentario
 */
-
-alert("Demostración de comentario")
+ document.getElementById("varios").innerHTML = "Mi nombre es: " + mi_nombre + " </br> " + "Mi dirección es: " + persona.direccion + " </br> " + "Mi edad es: " + mi_edad
